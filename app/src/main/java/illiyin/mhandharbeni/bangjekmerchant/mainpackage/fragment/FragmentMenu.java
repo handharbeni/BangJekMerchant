b07ac85e03ccfd0b189293800986beef0a5e9414
Faiz Al-Qurni <faizalqurni@gmailcom>
2017-12-06 16:29:41 +0700

    Update Layout Main

--- app/src/main/java/illiyin/mhandharbeni/bangjekmerchant/mainpackage/fragment/FragmentMenu.java
+++ app/src/main/java/illiyin/mhandharbeni/bangjekmerchant/mainpackage/fragment/FragmentMenu.java
@@ -1,12 +1,15 @@
 package illiyin.mhandharbeni.bangjekmerchant.mainpackage.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import illiyin.mhandharbeni.bangjekmerchant.R;
+import illiyin.mhandharbeni.bangjekmerchant.mainpackage.subactivity.DetailMenu;
 
 /**
  * Created by root on 12/5/17.
@@ -14,10 +17,35 @@ import illiyin.mhandharbeni.bangjekmerchant.R;
 
 public class FragmentMenu extends Fragment {
     View v;
-
+    private Button btnAddMenu;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.layout_fragment_menu, container, false);
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fetch_modules();
+        fetch_components();
+        fetch_click();
+    }
+
+    private void fetch_components(){
+        btnAddMenu = v.findViewById(R.id.btnAddMenu);
+    }
+    private void fetch_modules(){
+
+    }
+    private void fetch_click(){
+        btnAddMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity().getApplicationContext(), DetailMenu.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getActivity().startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/illiyin/mhandharbeni/bangjekmerchant/mainpackage/subactivity/DetailMenu.java
@@ -0,0 +1,39 @@
+package illiyin.mhandharbeni.bangjekmerchant.mainpackage.subactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import illiyin.mhandharbeni.bangjekmerchant.R;
+import illiyin.mhandharbeni.databasemodule.AdapterModel;
+
+/**
+ * Created by faizalqurni on 12/6/17.
+ */
+
+public class DetailMenu extends AppCompatActivity {
+    private AdapterModel adapterModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_form_menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fetch_modules();
+        fetch_components();
+        fetch_click();
+    }
+
+    private void fetch_components(){
+
+    }
+    private void fetch_modules(){
+
+    }
+    private void fetch_click(){
+
+    }
+}
